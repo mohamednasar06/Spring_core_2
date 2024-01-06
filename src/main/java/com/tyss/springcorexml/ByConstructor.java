db41236572d8590fe7f2d56584c64504c74deb6e
nasar <mdnasar647138@gmail.com>
2024-01-06 16:24:03 +0530

    first commit

---
+++ src/main/java/com/tyss/springcorexml/ByConstructor.java
@@ -0,0 +1,15 @@
+package com.tyss.springcorexml;
+
+public class ByConstructor {
+	
+	int a;
+	String name;
+	public ByConstructor(int a, String name) {
+		super();
+		this.a = a;
+		this.name = name;
+	}
+	
+	
+
+}
